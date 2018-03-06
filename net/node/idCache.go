U-Network/UNetwork

Relay the TX and Consensus
MSG should be relayed since not all node are connected with each other.

net/node/idCache.go
@@ -2,24 +2,45 @@ package node
 
 import (
 	"UGCNetwork/common"
+	"UGCNetwork/net/protocol"
 	"sync"
 )
 
 type idCache struct {
 	sync.RWMutex
-	list map[common.Uint256]bool
+	lastid    common.Uint256
+	index     int
+	idarray   []common.Uint256
+	idmaplsit map[common.Uint256]int
 }
 
 func (c *idCache) init() {
+	c.index = 0
+	c.idmaplsit = make(map[common.Uint256]int, protocol.MAXIDCACHED)
+	c.idarray = make([]common.Uint256, protocol.MAXIDCACHED)
 }
 
-func (c *idCache) add() {
-}
+func (c *idCache) add(id common.Uint256) {
+	oldid := c.idarray[c.index]
+	delete(c.idmaplsit, oldid)
+	c.idarray[c.index] = id
+	c.idmaplsit[id] = c.index
+	c.index++
+	c.lastid = id
+	c.index = c.index % protocol.MAXIDCACHED
 
-func (c *idCache) del() {
 }
 
 func (c *idCache) ExistedID(id common.Uint256) bool {
-	// TODO
+	c.Lock()
+	defer c.Unlock()
+	if id == c.lastid {
+		return true
+	}
+	if _, ok := c.idmaplsit[id]; ok {
+		return true
+	} else {
+		c.add(id)
+	}
 	return false
 }

AndreasAhrens/ft-blockchain

Signed-off-by: gongf05 <fang.ucla@gmail.com>

net/node/idCache.go
@@ -2,24 +2,45 @@ package node
 
 import (
 	"DNA/common"
+	"DNA/net/protocol"
 	"sync"
 )
 
 type idCache struct {
 	sync.RWMutex
-	list map[common.Uint256]bool
+	lastid    common.Uint256
+	index     int
+	idarray   []common.Uint256
+	idmaplsit map[common.Uint256]int
 }
 
 func (c *idCache) init() {
+	c.index = 0
+	c.idmaplsit = make(map[common.Uint256]int, protocol.MAXIDCACHED)
+	c.idarray = make([]common.Uint256, protocol.MAXIDCACHED)
 }
 
-func (c *idCache) add() {
-}
+func (c *idCache) add(id common.Uint256) {
+	oldid := c.idarray[c.index]
+	delete(c.idmaplsit, oldid)
+	c.idarray[c.index] = id
+	c.idmaplsit[id] = c.index
+	c.index++
+	c.lastid = id
+	c.index = c.index % protocol.MAXIDCACHED
 
-func (c *idCache) del() {
 }
 
 func (c *idCache) ExistedID(id common.Uint256) bool {
-	// TODO
+	c.Lock()
+	defer c.Unlock()
+	if id == c.lastid {
+		return true
+	}
+	if _, ok := c.idmaplsit[id]; ok {
+		return true
+	} else {
+		c.add(id)
+	}
 	return false
 }