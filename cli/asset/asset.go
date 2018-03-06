U-Network/UNetwork

sync data to wallet when wallet height less than block height

cli/asset/asset.go
@@ -1,23 +1,30 @@
 package asset
 
 import (
-	"UGCNetwork/account"
-	. "UGCNetwork/cli/common"
-	. "UGCNetwork/common"
-	"UGCNetwork/net/httpjsonrpc"
-	"UGCNetwork/sdk"
+	"bytes"
 	"fmt"
 	"math/rand"
 	"os"
 
+	"UGCNetwork/account"
+	. "UGCNetwork/cli/common"
+	. "UGCNetwork/common"
+	"UGCNetwork/core/asset"
 	"UGCNetwork/core/transaction"
-	"bytes"
+	"UGCNetwork/net/httpjsonrpc"
+	"UGCNetwork/sdk"
+
 	"github.com/urfave/cli"
 )
 
 const (
 	RANDBYTELEN = 4
 )
+
+func valuetoFixed64(value float64) Fixed64 {
+	value *= asset.AssetPrecisionExpand
+	return Fixed64(value)
+}
 
 func openWallet(name string, passwd []byte) account.Client {
 	if name == account.WalletFileName {
@@ -43,18 +50,16 @@ func assetAction(c *cli.Context) error {
 		cli.ShowSubcommandHelp(c)
 		return nil
 	}
-
-	wallet := openWallet(c.String("wallet"), WalletPassword(c.String("password")))
-	value := c.Int64("value")
+	value := valuetoFixed64(c.Float64("value"))
 	if value == 0 {
 		fmt.Println("invalid value [--value]")
 		return nil
 	}
 
+	wallet := openWallet(c.String("wallet"), WalletPassword(c.String("password")))
 	var txn *transaction.Transaction
 	var buffer bytes.Buffer
 	var err error
-
 	if reg {
 		name := c.String("name")
 		if name == "" {
@@ -62,7 +67,7 @@ func assetAction(c *cli.Context) error {
 			rand.Read(rbuf)
 			name = "UGCNetwork-" + ToHexString(rbuf)
 		}
-		txn, err = sdk.MakeRegTransaction(wallet, name, Fixed64(value))
+		txn, err = sdk.MakeRegTransaction(wallet, name, value)
 	} else {
 		asset := c.String("asset")
 		address := c.String("to")
@@ -72,9 +77,9 @@ func assetAction(c *cli.Context) error {
 		}
 		assetID, _ := StringToUint256(asset)
 		if issue {
-			txn, err = sdk.MakeIssueTransaction(wallet, assetID, address, Fixed64(value))
+			txn, err = sdk.MakeIssueTransaction(wallet, assetID, address, value)
 		} else if transfer {
-			txn, err = sdk.MakeTransferTransaction(wallet, assetID, address, Fixed64(value))
+			txn, err = sdk.MakeTransferTransaction(wallet, assetID, address, value)
 		}
 	}
 	if err != nil {
@@ -135,9 +140,9 @@ func NewCommand() *cli.Command {
 				Name:  "to",
 				Usage: "asset to whom",
 			},
-			cli.Int64Flag{
+			cli.Float64Flag{
 				Name:  "value, v",
-				Usage: "asset ammount",
+				Usage: "asset amount",
 			},
 		},
 		Action: assetAction,

net/httpjsonrpc/interfaces.go
@@ -1,15 +1,6 @@
 package httpjsonrpc
 
 import (
-	"UGCNetwork/account"
-	. "UGCNetwork/common"
-	"UGCNetwork/common/config"
-	"UGCNetwork/common/log"
-	"UGCNetwork/core/ledger"
-	tx "UGCNetwork/core/transaction"
-	"UGCNetwork/core/transaction/payload"
-	. "UGCNetwork/errors"
-	"UGCNetwork/sdk"
 	"bytes"
 	"encoding/base64"
 	"encoding/hex"
@@ -18,6 +9,17 @@ import (
 	"os"
 	"path/filepath"
 	"time"
+
+	"UGCNetwork/account"
+	. "UGCNetwork/common"
+	"UGCNetwork/common/config"
+	"UGCNetwork/common/log"
+	"UGCNetwork/core/asset"
+	"UGCNetwork/core/ledger"
+	tx "UGCNetwork/core/transaction"
+	"UGCNetwork/core/transaction/payload"
+	. "UGCNetwork/errors"
+	"UGCNetwork/sdk"
 
 	"github.com/mitchellh/go-homedir"
 )
@@ -62,8 +64,9 @@ func TransArryByteToHexString(ptx *tx.Transaction) *Transactions {
 	trans.Outputs = make([]TxoutputInfo, len(ptx.Outputs))
 	for _, v := range ptx.Outputs {
 		trans.Outputs[n].AssetID = ToHexString(v.AssetID.ToArray())
-		trans.Outputs[n].Value = v.Value
-		trans.Outputs[n].ProgramHash = ToHexString(v.ProgramHash.ToArray())
+		trans.Outputs[n].Value = float64(v.Value) / asset.AssetPrecisionExpand
+		address, _ := v.ProgramHash.ToAddress()
+		trans.Outputs[n].Address = address
 		n++
 	}
 
@@ -82,8 +85,9 @@ func TransArryByteToHexString(ptx *tx.Transaction) *Transactions {
 		trans.AssetOutputs[n].Txout = make([]TxoutputInfo, len(v))
 		for m := 0; m < len(v); m++ {
 			trans.AssetOutputs[n].Txout[m].AssetID = ToHexString(v[m].AssetID.ToArray())
-			trans.AssetOutputs[n].Txout[m].Value = v[m].Value
-			trans.AssetOutputs[n].Txout[m].ProgramHash = ToHexString(v[m].ProgramHash.ToArray())
+			trans.AssetOutputs[n].Txout[m].Value = float64(v[m].Value) / asset.AssetPrecisionExpand
+			address, _ := v[m].ProgramHash.ToAddress()
+			trans.AssetOutputs[n].Txout[m].Address = address
 		}
 		n += 1
 	}
@@ -762,7 +766,7 @@ func deleteAccount(params []interface{}) map[string]interface{} {
 	if err := walletInstance.DeleteContract(pbUint160); err != nil {
 		return UgcNetworkRpc("Delete contract error:" + err.Error())
 	}
-	if err := walletInstance.DeleteCoins(pbUint160); err != nil {
+	if err := walletInstance.DeleteCoinsData(pbUint160); err != nil {
 		return UgcNetworkRpc("Delete coins error:" + err.Error())
 	}
 

AndreasAhrens/ft-blockchain

Signed-off-by: gongf05 <fang.ucla@gmail.com>

cli/asset/asset.go
@@ -1,23 +1,30 @@
 package asset
 
 import (
-	"DNA/account"
-	. "DNA/cli/common"
-	. "DNA/common"
-	"DNA/net/httpjsonrpc"
-	"DNA/sdk"
+	"bytes"
 	"fmt"
 	"math/rand"
 	"os"
 
+	"DNA/account"
+	. "DNA/cli/common"
+	. "DNA/common"
+	"DNA/core/asset"
 	"DNA/core/transaction"
-	"bytes"
+	"DNA/net/httpjsonrpc"
+	"DNA/sdk"
+
 	"github.com/urfave/cli"
 )
 
 const (
 	RANDBYTELEN = 4
 )
+
+func valuetoFixed64(value float64) Fixed64 {
+	value *= asset.AssetPrecisionExpand
+	return Fixed64(value)
+}
 
 func openWallet(name string, passwd []byte) account.Client {
 	if name == account.WalletFileName {
@@ -43,18 +50,16 @@ func assetAction(c *cli.Context) error {
 		cli.ShowSubcommandHelp(c)
 		return nil
 	}
-
-	wallet := openWallet(c.String("wallet"), WalletPassword(c.String("password")))
-	value := c.Int64("value")
+	value := valuetoFixed64(c.Float64("value"))
 	if value == 0 {
 		fmt.Println("invalid value [--value]")
 		return nil
 	}
 
+	wallet := openWallet(c.String("wallet"), WalletPassword(c.String("password")))
 	var txn *transaction.Transaction
 	var buffer bytes.Buffer
 	var err error
-
 	if reg {
 		name := c.String("name")
 		if name == "" {
@@ -62,7 +67,7 @@ func assetAction(c *cli.Context) error {
 			rand.Read(rbuf)
 			name = "DNA-" + ToHexString(rbuf)
 		}
-		txn, err = sdk.MakeRegTransaction(wallet, name, Fixed64(value))
+		txn, err = sdk.MakeRegTransaction(wallet, name, value)
 	} else {
 		asset := c.String("asset")
 		address := c.String("to")
@@ -72,9 +77,9 @@ func assetAction(c *cli.Context) error {
 		}
 		assetID, _ := StringToUint256(asset)
 		if issue {
-			txn, err = sdk.MakeIssueTransaction(wallet, assetID, address, Fixed64(value))
+			txn, err = sdk.MakeIssueTransaction(wallet, assetID, address, value)
 		} else if transfer {
-			txn, err = sdk.MakeTransferTransaction(wallet, assetID, address, Fixed64(value))
+			txn, err = sdk.MakeTransferTransaction(wallet, assetID, address, value)
 		}
 	}
 	if err != nil {
@@ -135,9 +140,9 @@ func NewCommand() *cli.Command {
 				Name:  "to",
 				Usage: "asset to whom",
 			},
-			cli.Int64Flag{
+			cli.Float64Flag{
 				Name:  "value, v",
-				Usage: "asset ammount",
+				Usage: "asset amount",
 			},
 		},
 		Action: assetAction,

net/httpjsonrpc/interfaces.go
@@ -1,15 +1,6 @@
 package httpjsonrpc
 
 import (
-	"DNA/account"
-	. "DNA/common"
-	"DNA/common/config"
-	"DNA/common/log"
-	"DNA/core/ledger"
-	tx "DNA/core/transaction"
-	"DNA/core/transaction/payload"
-	. "DNA/errors"
-	"DNA/sdk"
 	"bytes"
 	"encoding/base64"
 	"encoding/hex"
@@ -18,6 +9,17 @@ import (
 	"os"
 	"path/filepath"
 	"time"
+
+	"DNA/account"
+	. "DNA/common"
+	"DNA/common/config"
+	"DNA/common/log"
+	"DNA/core/asset"
+	"DNA/core/ledger"
+	tx "DNA/core/transaction"
+	"DNA/core/transaction/payload"
+	. "DNA/errors"
+	"DNA/sdk"
 
 	"github.com/mitchellh/go-homedir"
 )
@@ -62,8 +64,9 @@ func TransArryByteToHexString(ptx *tx.Transaction) *Transactions {
 	trans.Outputs = make([]TxoutputInfo, len(ptx.Outputs))
 	for _, v := range ptx.Outputs {
 		trans.Outputs[n].AssetID = ToHexString(v.AssetID.ToArray())
-		trans.Outputs[n].Value = v.Value
-		trans.Outputs[n].ProgramHash = ToHexString(v.ProgramHash.ToArray())
+		trans.Outputs[n].Value = float64(v.Value) / asset.AssetPrecisionExpand
+		address, _ := v.ProgramHash.ToAddress()
+		trans.Outputs[n].Address = address
 		n++
 	}
 
@@ -82,8 +85,9 @@ func TransArryByteToHexString(ptx *tx.Transaction) *Transactions {
 		trans.AssetOutputs[n].Txout = make([]TxoutputInfo, len(v))
 		for m := 0; m < len(v); m++ {
 			trans.AssetOutputs[n].Txout[m].AssetID = ToHexString(v[m].AssetID.ToArray())
-			trans.AssetOutputs[n].Txout[m].Value = v[m].Value
-			trans.AssetOutputs[n].Txout[m].ProgramHash = ToHexString(v[m].ProgramHash.ToArray())
+			trans.AssetOutputs[n].Txout[m].Value = float64(v[m].Value) / asset.AssetPrecisionExpand
+			address, _ := v[m].ProgramHash.ToAddress()
+			trans.AssetOutputs[n].Txout[m].Address = address
 		}
 		n += 1
 	}
@@ -762,7 +766,7 @@ func deleteAccount(params []interface{}) map[string]interface{} {
 	if err := walletInstance.DeleteContract(pbUint160); err != nil {
 		return DnaRpc("Delete contract error:" + err.Error())
 	}
-	if err := walletInstance.DeleteCoins(pbUint160); err != nil {
+	if err := walletInstance.DeleteCoinsData(pbUint160); err != nil {
 		return DnaRpc("Delete coins error:" + err.Error())
 	}
 