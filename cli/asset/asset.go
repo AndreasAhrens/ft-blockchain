U-Network/UNetwork

Add lock asset feature

cli/asset/asset.go
@@ -31,66 +31,114 @@ func openWallet(name string, passwd []byte) account.Client {
 	}
 	return wallet
 }
+func parseAssetName(c *cli.Context) string {
+	name := c.String("name")
+	if name == "" {
+		rbuf := make([]byte, RANDBYTELEN)
+		rand.Read(rbuf)
+		name = "UGCNetwork-" + BytesToHexString(rbuf)
+	}
+
+	return name
+}
+
+func parseAssetID(c *cli.Context) Uint256 {
+	asset := c.String("asset")
+	if asset == "" {
+		fmt.Println("missing flag [--asset]")
+		os.Exit(1)
+	}
+	var assetBytes []byte
+	var assetID Uint256
+	assetBytes, err := HexStringToBytesReverse(asset)
+	if err != nil {
+		fmt.Println("invalid asset ID")
+		os.Exit(1)
+	}
+	if err := assetID.Deserialize(bytes.NewReader(assetBytes)); err != nil {
+		fmt.Println("invalid asset hash")
+		os.Exit(1)
+	}
+
+	return assetID
+}
+
+func parseAddress(c *cli.Context) string {
+	if address := c.String("to"); address != "" {
+		_, err := ToScriptHash(address)
+		if err != nil {
+			fmt.Println("invalid receiver address")
+			os.Exit(1)
+		}
+		return address
+	} else {
+		fmt.Println("missing flag [--to]")
+		os.Exit(1)
+	}
+
+	return ""
+}
+
+func parseHeight(c *cli.Context) int64 {
+	height := c.Int64("height")
+	if height != -1 {
+		return height
+	} else {
+		fmt.Println("invalid parameter [--height]")
+		os.Exit(1)
+	}
+
+	return 0
+}
 
 func assetAction(c *cli.Context) error {
 	if c.NumFlags() == 0 {
 		cli.ShowSubcommandHelp(c)
 		return nil
 	}
-	reg := c.Bool("reg")
-	issue := c.Bool("issue")
-	transfer := c.Bool("transfer")
-	if !reg && !issue && !transfer {
-		cli.ShowSubcommandHelp(c)
-		return nil
-	}
+
 	value := c.String("value")
 	if value == "" {
 		fmt.Println("asset amount is required with [--value]")
 		return nil
 	}
 
-	wallet := openWallet(c.String("wallet"), WalletPassword(c.String("password")))
 	var txn *transaction.Transaction
 	var buffer bytes.Buffer
 	var err error
-	if reg {
-		name := c.String("name")
-		if name == "" {
-			rbuf := make([]byte, RANDBYTELEN)
-			rand.Read(rbuf)
-			name = "UGCNetwork-" + BytesToHexString(rbuf)
-		}
+	switch {
+	case c.Bool("reg"):
+		name := parseAssetName(c)
+		wallet := openWallet(c.String("wallet"), WalletPassword(c.String("password")))
 		txn, err = sdk.MakeRegTransaction(wallet, name, value)
-	} else {
-		asset := c.String("asset")
-		address := c.String("to")
-		if asset == "" || address == "" {
-			fmt.Println("missing flag [--asset] or [--to]")
-			return nil
-		}
-		var assetBytes []byte
-		var assetID Uint256
-		assetBytes, err = HexStringToBytesReverse(asset)
+	case c.Bool("issue"):
+		assetID := parseAssetID(c)
+		address := parseAddress(c)
+		wallet := openWallet(c.String("wallet"), WalletPassword(c.String("password")))
+		txn, err = sdk.MakeIssueTransaction(wallet, assetID, address, value)
+	case c.Bool("transfer"):
+		assetID := c.String("asset")
+		address := parseAddress(c)
+		resp, err := httpjsonrpc.Call(Address(), "sendtoaddress", 0, []interface{}{assetID, address, value})
 		if err != nil {
-			fmt.Println("invalid asset ID")
-			return nil
+			fmt.Fprintln(os.Stderr, err)
+			return err
 		}
-		if err = assetID.Deserialize(bytes.NewReader(assetBytes)); err != nil {
-			fmt.Println("invalid asset hash")
-			return nil
+		FormatOutput(resp)
+		return nil
+	case c.Bool("lock"):
+		assetID := c.String("asset")
+		height := parseHeight(c)
+		resp, err := httpjsonrpc.Call(Address(), "lockasset", 0, []interface{}{assetID, value, height})
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return err
 		}
-		if issue {
-			txn, err = sdk.MakeIssueTransaction(wallet, assetID, address, value)
-		} else if transfer {
-			resp, err := httpjsonrpc.Call(Address(), "sendtoaddress", 0, []interface{}{asset, address, value})
-			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
-				return err
-			}
-			FormatOutput(resp)
-			return nil
-		}
+		FormatOutput(resp)
+		return nil
+	default:
+		cli.ShowSubcommandHelp(c)
+		return nil
 	}
 	if err != nil {
 		fmt.Println(err)
@@ -129,6 +177,10 @@ func NewCommand() *cli.Command {
 				Name:  "transfer, t",
 				Usage: "transfer asset",
 			},
+			cli.BoolFlag{
+				Name:  "lock",
+				Usage: "lock asset",
+			},
 			cli.StringFlag{
 				Name:  "wallet, w",
 				Usage: "wallet name",
@@ -154,6 +206,11 @@ func NewCommand() *cli.Command {
 				Name:  "value, v",
 				Usage: "asset amount",
 				Value: "",
+			},
+			cli.Int64Flag{
+				Name:  "height",
+				Usage: "asset lock height",
+				Value: -1,
 			},
 		},
 		Action: assetAction,

core/asset/LockedAsset.go
@@ -1,0 +1,49 @@
+package asset
+
+import (
+	"io"
+
+	. "UGCNetwork/common"
+	"UGCNetwork/common/serialization"
+)
+
+type LockAsset struct {
+	Lock   uint32
+	Unlock uint32
+	Amount Fixed64
+}
+
+func (a *LockAsset) Serialize(w io.Writer) error {
+	if err := serialization.WriteUint32(w, a.Lock); err != nil {
+		return err
+	}
+	if err := serialization.WriteUint32(w, a.Unlock); err != nil {
+		return err
+	}
+	if err := a.Amount.Serialize(w); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *LockAsset) Deserialize(r io.Reader) error {
+	startHeight, err := serialization.ReadUint32(r)
+	if err != nil {
+		return err
+	}
+	a.Lock = startHeight
+
+	endHeight, err := serialization.ReadUint32(r)
+	if err != nil {
+		return err
+	}
+	a.Unlock = endHeight
+
+	a.Amount = *new(Fixed64)
+	if err := a.Amount.Deserialize(r); err != nil {
+		return err
+	}
+
+	return nil
+}

core/transaction/payload/LockAsset.go
@@ -1,0 +1,65 @@
+package payload
+
+import (
+	"io"
+
+	. "UGCNetwork/common"
+	"UGCNetwork/common/serialization"
+)
+
+type LockAsset struct {
+	ProgramHash  Uint160
+	AssetID      Uint256
+	Amount       Fixed64
+	UnlockHeight uint32
+}
+
+func (p *LockAsset) Data(version byte) []byte {
+	return []byte{0}
+}
+
+func (p *LockAsset) Serialize(w io.Writer, version byte) error {
+	_, err := p.ProgramHash.Serialize(w)
+	if err != nil {
+		return err
+	}
+	_, err = p.AssetID.Serialize(w)
+	if err != nil {
+		return err
+	}
+	if err := p.Amount.Serialize(w); err != nil {
+		return err
+	}
+	if err := serialization.WriteUint32(w, p.UnlockHeight); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *LockAsset) Deserialize(r io.Reader, version byte) error {
+	if err := p.ProgramHash.Deserialize(r); err != nil {
+		return err
+	}
+	if err := p.AssetID.Deserialize(r); err != nil {
+		return err
+	}
+	if err := p.Amount.Deserialize(r); err != nil {
+		return err
+	}
+	height, err := serialization.ReadUint32(r)
+	if err != nil {
+		return err
+	}
+	p.UnlockHeight = height
+
+	return nil
+}
+
+func (p *LockAsset) ToString() string {
+	str := ""
+	str += BytesToHexString(p.ProgramHash.ToArray())
+	str += BytesToHexString(p.AssetID.ToArray())
+
+	return str
+}

net/httpjsonrpc/interfaces.go
@@ -274,7 +274,7 @@ func sendRawTransaction(params []interface{}) map[string]interface{} {
 		if err := txn.Deserialize(bytes.NewReader(hex)); err != nil {
 			return UgcNetworkRpcInvalidTransaction
 		}
-		if txn.TxType != tx.TransferAsset && txn.TxType != tx.BookKeeper {
+		if txn.TxType != tx.TransferAsset && txn.TxType != tx.LockAsset && txn.TxType != tx.BookKeeper {
 			return UgcNetworkRpc("invalid transaction type")
 		}
 		hash = txn.Hash()
@@ -843,6 +843,60 @@ func sendToAddress(params []interface{}) map[string]interface{} {
 	}
 	txHash := txn.Hash()
 	return UgcNetworkRpc(BytesToHexString(txHash.ToArrayReverse()))
+}
+
+func lockAsset(params []interface{}) map[string]interface{} {
+	if len(params) < 3 {
+		return UgcNetworkRpcNil
+	}
+	var asset, value string
+	var height float64
+	switch params[0].(type) {
+	case string:
+		asset = params[0].(string)
+	default:
+		return UgcNetworkRpcInvalidParameter
+	}
+	switch params[1].(type) {
+	case string:
+		value = params[1].(string)
+	default:
+		return UgcNetworkRpcInvalidParameter
+	}
+	switch params[2].(type) {
+	case float64:
+		height = params[2].(float64)
+	default:
+		return UgcNetworkRpcInvalidParameter
+	}
+	if Wallet == nil {
+		return UgcNetworkRpc("error: invalid wallet instance")
+	}
+
+	accts := Wallet.GetAccounts()
+	if len(accts) > 1 {
+		return UgcNetworkRpc("error: does't support multi-addresses wallet locking asset")
+	}
+
+	tmp, err := HexStringToBytesReverse(asset)
+	if err != nil {
+		return UgcNetworkRpc("error: invalid asset ID")
+	}
+	var assetID Uint256
+	if err := assetID.Deserialize(bytes.NewReader(tmp)); err != nil {
+		return UgcNetworkRpc("error: invalid asset hash")
+	}
+
+	txn, err := sdk.MakeLockAssetTransaction(Wallet, assetID, value, uint32(height))
+	if err != nil {
+		return UgcNetworkRpc("error: " + err.Error())
+	}
+
+	txnHash := txn.Hash()
+	if errCode := VerifyAndSendTx(txn); errCode != ErrNoError {
+		return UgcNetworkRpc(errCode.Error())
+	}
+	return UgcNetworkRpc(BytesToHexString(txnHash.ToArrayReverse()))
 }
 
 func signMultisigTransaction(params []interface{}) map[string]interface{} {

AndreasAhrens/ft-blockchain

Signed-off-by: gongf05 <fang.ucla@gmail.com>

cli/asset/asset.go
@@ -31,66 +31,114 @@ func openWallet(name string, passwd []byte) account.Client {
 	}
 	return wallet
 }
+func parseAssetName(c *cli.Context) string {
+	name := c.String("name")
+	if name == "" {
+		rbuf := make([]byte, RANDBYTELEN)
+		rand.Read(rbuf)
+		name = "DNA-" + BytesToHexString(rbuf)
+	}
+
+	return name
+}
+
+func parseAssetID(c *cli.Context) Uint256 {
+	asset := c.String("asset")
+	if asset == "" {
+		fmt.Println("missing flag [--asset]")
+		os.Exit(1)
+	}
+	var assetBytes []byte
+	var assetID Uint256
+	assetBytes, err := HexStringToBytesReverse(asset)
+	if err != nil {
+		fmt.Println("invalid asset ID")
+		os.Exit(1)
+	}
+	if err := assetID.Deserialize(bytes.NewReader(assetBytes)); err != nil {
+		fmt.Println("invalid asset hash")
+		os.Exit(1)
+	}
+
+	return assetID
+}
+
+func parseAddress(c *cli.Context) string {
+	if address := c.String("to"); address != "" {
+		_, err := ToScriptHash(address)
+		if err != nil {
+			fmt.Println("invalid receiver address")
+			os.Exit(1)
+		}
+		return address
+	} else {
+		fmt.Println("missing flag [--to]")
+		os.Exit(1)
+	}
+
+	return ""
+}
+
+func parseHeight(c *cli.Context) int64 {
+	height := c.Int64("height")
+	if height != -1 {
+		return height
+	} else {
+		fmt.Println("invalid parameter [--height]")
+		os.Exit(1)
+	}
+
+	return 0
+}
 
 func assetAction(c *cli.Context) error {
 	if c.NumFlags() == 0 {
 		cli.ShowSubcommandHelp(c)
 		return nil
 	}
-	reg := c.Bool("reg")
-	issue := c.Bool("issue")
-	transfer := c.Bool("transfer")
-	if !reg && !issue && !transfer {
-		cli.ShowSubcommandHelp(c)
-		return nil
-	}
+
 	value := c.String("value")
 	if value == "" {
 		fmt.Println("asset amount is required with [--value]")
 		return nil
 	}
 
-	wallet := openWallet(c.String("wallet"), WalletPassword(c.String("password")))
 	var txn *transaction.Transaction
 	var buffer bytes.Buffer
 	var err error
-	if reg {
-		name := c.String("name")
-		if name == "" {
-			rbuf := make([]byte, RANDBYTELEN)
-			rand.Read(rbuf)
-			name = "DNA-" + BytesToHexString(rbuf)
-		}
+	switch {
+	case c.Bool("reg"):
+		name := parseAssetName(c)
+		wallet := openWallet(c.String("wallet"), WalletPassword(c.String("password")))
 		txn, err = sdk.MakeRegTransaction(wallet, name, value)
-	} else {
-		asset := c.String("asset")
-		address := c.String("to")
-		if asset == "" || address == "" {
-			fmt.Println("missing flag [--asset] or [--to]")
-			return nil
-		}
-		var assetBytes []byte
-		var assetID Uint256
-		assetBytes, err = HexStringToBytesReverse(asset)
+	case c.Bool("issue"):
+		assetID := parseAssetID(c)
+		address := parseAddress(c)
+		wallet := openWallet(c.String("wallet"), WalletPassword(c.String("password")))
+		txn, err = sdk.MakeIssueTransaction(wallet, assetID, address, value)
+	case c.Bool("transfer"):
+		assetID := c.String("asset")
+		address := parseAddress(c)
+		resp, err := httpjsonrpc.Call(Address(), "sendtoaddress", 0, []interface{}{assetID, address, value})
 		if err != nil {
-			fmt.Println("invalid asset ID")
-			return nil
+			fmt.Fprintln(os.Stderr, err)
+			return err
 		}
-		if err = assetID.Deserialize(bytes.NewReader(assetBytes)); err != nil {
-			fmt.Println("invalid asset hash")
-			return nil
+		FormatOutput(resp)
+		return nil
+	case c.Bool("lock"):
+		assetID := c.String("asset")
+		height := parseHeight(c)
+		resp, err := httpjsonrpc.Call(Address(), "lockasset", 0, []interface{}{assetID, value, height})
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return err
 		}
-		if issue {
-			txn, err = sdk.MakeIssueTransaction(wallet, assetID, address, value)
-		} else if transfer {
-			resp, err := httpjsonrpc.Call(Address(), "sendtoaddress", 0, []interface{}{asset, address, value})
-			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
-				return err
-			}
-			FormatOutput(resp)
-			return nil
-		}
+		FormatOutput(resp)
+		return nil
+	default:
+		cli.ShowSubcommandHelp(c)
+		return nil
 	}
 	if err != nil {
 		fmt.Println(err)
@@ -129,6 +177,10 @@ func NewCommand() *cli.Command {
 				Name:  "transfer, t",
 				Usage: "transfer asset",
 			},
+			cli.BoolFlag{
+				Name:  "lock",
+				Usage: "lock asset",
+			},
 			cli.StringFlag{
 				Name:  "wallet, w",
 				Usage: "wallet name",
@@ -154,6 +206,11 @@ func NewCommand() *cli.Command {
 				Name:  "value, v",
 				Usage: "asset amount",
 				Value: "",
+			},
+			cli.Int64Flag{
+				Name:  "height",
+				Usage: "asset lock height",
+				Value: -1,
 			},
 		},
 		Action: assetAction,

core/asset/LockedAsset.go
@@ -1,0 +1,49 @@
+package asset
+
+import (
+	"io"
+
+	. "DNA/common"
+	"DNA/common/serialization"
+)
+
+type LockAsset struct {
+	Lock   uint32
+	Unlock uint32
+	Amount Fixed64
+}
+
+func (a *LockAsset) Serialize(w io.Writer) error {
+	if err := serialization.WriteUint32(w, a.Lock); err != nil {
+		return err
+	}
+	if err := serialization.WriteUint32(w, a.Unlock); err != nil {
+		return err
+	}
+	if err := a.Amount.Serialize(w); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *LockAsset) Deserialize(r io.Reader) error {
+	startHeight, err := serialization.ReadUint32(r)
+	if err != nil {
+		return err
+	}
+	a.Lock = startHeight
+
+	endHeight, err := serialization.ReadUint32(r)
+	if err != nil {
+		return err
+	}
+	a.Unlock = endHeight
+
+	a.Amount = *new(Fixed64)
+	if err := a.Amount.Deserialize(r); err != nil {
+		return err
+	}
+
+	return nil
+}

core/transaction/payload/LockAsset.go
@@ -1,0 +1,65 @@
+package payload
+
+import (
+	"io"
+
+	. "DNA/common"
+	"DNA/common/serialization"
+)
+
+type LockAsset struct {
+	ProgramHash  Uint160
+	AssetID      Uint256
+	Amount       Fixed64
+	UnlockHeight uint32
+}
+
+func (p *LockAsset) Data(version byte) []byte {
+	return []byte{0}
+}
+
+func (p *LockAsset) Serialize(w io.Writer, version byte) error {
+	_, err := p.ProgramHash.Serialize(w)
+	if err != nil {
+		return err
+	}
+	_, err = p.AssetID.Serialize(w)
+	if err != nil {
+		return err
+	}
+	if err := p.Amount.Serialize(w); err != nil {
+		return err
+	}
+	if err := serialization.WriteUint32(w, p.UnlockHeight); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *LockAsset) Deserialize(r io.Reader, version byte) error {
+	if err := p.ProgramHash.Deserialize(r); err != nil {
+		return err
+	}
+	if err := p.AssetID.Deserialize(r); err != nil {
+		return err
+	}
+	if err := p.Amount.Deserialize(r); err != nil {
+		return err
+	}
+	height, err := serialization.ReadUint32(r)
+	if err != nil {
+		return err
+	}
+	p.UnlockHeight = height
+
+	return nil
+}
+
+func (p *LockAsset) ToString() string {
+	str := ""
+	str += BytesToHexString(p.ProgramHash.ToArray())
+	str += BytesToHexString(p.AssetID.ToArray())
+
+	return str
+}

net/httpjsonrpc/interfaces.go
@@ -274,7 +274,7 @@ func sendRawTransaction(params []interface{}) map[string]interface{} {
 		if err := txn.Deserialize(bytes.NewReader(hex)); err != nil {
 			return DnaRpcInvalidTransaction
 		}
-		if txn.TxType != tx.TransferAsset && txn.TxType != tx.BookKeeper {
+		if txn.TxType != tx.TransferAsset && txn.TxType != tx.LockAsset && txn.TxType != tx.BookKeeper {
 			return DnaRpc("invalid transaction type")
 		}
 		hash = txn.Hash()
@@ -843,6 +843,60 @@ func sendToAddress(params []interface{}) map[string]interface{} {
 	}
 	txHash := txn.Hash()
 	return DnaRpc(BytesToHexString(txHash.ToArrayReverse()))
+}
+
+func lockAsset(params []interface{}) map[string]interface{} {
+	if len(params) < 3 {
+		return DnaRpcNil
+	}
+	var asset, value string
+	var height float64
+	switch params[0].(type) {
+	case string:
+		asset = params[0].(string)
+	default:
+		return DnaRpcInvalidParameter
+	}
+	switch params[1].(type) {
+	case string:
+		value = params[1].(string)
+	default:
+		return DnaRpcInvalidParameter
+	}
+	switch params[2].(type) {
+	case float64:
+		height = params[2].(float64)
+	default:
+		return DnaRpcInvalidParameter
+	}
+	if Wallet == nil {
+		return DnaRpc("error: invalid wallet instance")
+	}
+
+	accts := Wallet.GetAccounts()
+	if len(accts) > 1 {
+		return DnaRpc("error: does't support multi-addresses wallet locking asset")
+	}
+
+	tmp, err := HexStringToBytesReverse(asset)
+	if err != nil {
+		return DnaRpc("error: invalid asset ID")
+	}
+	var assetID Uint256
+	if err := assetID.Deserialize(bytes.NewReader(tmp)); err != nil {
+		return DnaRpc("error: invalid asset hash")
+	}
+
+	txn, err := sdk.MakeLockAssetTransaction(Wallet, assetID, value, uint32(height))
+	if err != nil {
+		return DnaRpc("error: " + err.Error())
+	}
+
+	txnHash := txn.Hash()
+	if errCode := VerifyAndSendTx(txn); errCode != ErrNoError {
+		return DnaRpc(errCode.Error())
+	}
+	return DnaRpc(BytesToHexString(txnHash.ToArrayReverse()))
 }
 
 func signMultisigTransaction(params []interface{}) map[string]interface{} {