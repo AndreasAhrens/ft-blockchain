U-Network/UNetwork

Return meaningful errcode when transaction verification failed

net/httpjsonrpc/interfaces.go
@@ -277,7 +277,7 @@ func sendRawTransaction(params []interface{}) map[string]interface{} {
 		}
 		hash = txn.Hash()
 		if errCode := VerifyAndSendTx(&txn); errCode != ErrNoError {
-			return UgcNetworkRpcInternalError
+			return UgcNetworkRpc(errCode.Error())
 		}
 	default:
 		return UgcNetworkRpcInvalidParameter

AndreasAhrens/ft-blockchain

Signed-off-by: gongf05 <fang.ucla@gmail.com>

net/httpjsonrpc/interfaces.go
@@ -277,7 +277,7 @@ func sendRawTransaction(params []interface{}) map[string]interface{} {
 		}
 		hash = txn.Hash()
 		if errCode := VerifyAndSendTx(&txn); errCode != ErrNoError {
-			return DnaRpcInternalError
+			return DnaRpc(errCode.Error())
 		}
 	default:
 		return DnaRpcInvalidParameter